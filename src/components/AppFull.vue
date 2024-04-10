8a9c1f8456355f2676b37fe2c56dabb488d261d6
hajjamn <hajjamn@gmail.com>
2024-04-10 14:35:31 +0200

    FIX: oops wrong folder

---
+++ src/components/AppFull.vue
@@ -0,0 +1,175 @@
+<script>
+</script>
+
+<template>
+
+<header class="page-header">
+        <div class="container">
+            <nav class="row">
+                <!-- LINKS TO STORE SECTIONS -->
+                <ul class="col-4 col-flex">
+                    <li><a href="#">Donna</a></li>
+                    <li><a href="#">Uomo</a></li>
+                    <li><a href="#">Bambini</a></li>
+                </ul>
+                <!-- LOGO -->
+                <div class="col=4">
+                    <a href=""><img src="/img/boolean-logo.png" alt="Logo boolean" class="logo"></a>
+                </div>
+                <!-- STORE NAV -->
+                <ul class="col=4 col-flex">
+                    <li><a href="#">&#9825;</a></li>
+                    <li><a href="#">&#128100;</a></li>
+                    <li><a href="#">&#128717;</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="page-content">
+        <section class="content-section">
+                <!-- PRODUCTS LIST -->
+            <div class="container">
+                <ul class="row">
+                    <!-- PRODUCT 1 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/1.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/1b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                                <li class="badge discount"><p>-50%</p></li>
+                                <li class="badge sustainable"><p>Sostenibilità</p></li>
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Levi's</p></a></li>
+                            <li class="product-name"><a href="#">RELAXED FIT TEE UNISEX</a></li>
+                            <li class="price"><span class="after-disc">14,99 &euro; </span><span class="before-disc">29,99 &euro;</span></li>
+                        </ul>
+                    </li>
+                    <!-- PRODUCT 2 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/2.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/2b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                                <li class="badge discount"><p>-30%</p></li>
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Guess</p></a></li>
+                            <li class="product-name"><a href="#">ROSES TEE</a></li>
+                            <li class="price"><span class="after-disc">20,99 &euro; </span><span class="before-disc">29,99 &euro;</span></li>
+                        </ul>
+                    </li>
+                    <!-- PRODUCT 3 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/3.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/3b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                                <li class="badge discount"><p>-30%</p></li>
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Come Zucchero Filato</p></a></li>
+                            <li class="product-name"><a href="#">VOGLIA DI COLORI PASTELLO</a></li>
+                            <li class="price"><span class="after-disc">129,99 &euro; </span><span class="before-disc">184,99 &euro;</span></li>
+                        </ul>
+                    </li>
+                    <!-- PRODUCT 4 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/4.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/4b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                                <li class="badge discount"><p>-50%</p></li>
+                                <li class="badge sustainable"><p>Sostenibilità</p></li>
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Levi's</p></a></li>
+                            <li class="product-name"><a href="#">TEE UNISEX</a></li>
+                            <li class="price"><span class="after-disc">14,99 &euro; </span><span class="before-disc">29,99 &euro;</span></li>
+                        </ul>
+                    </li>
+                    <!-- PRODUCT 5 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/5.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/5b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Maya Deluxe</p></a></li>
+                            <li class="product-name"><a href="#">STRIP BODICE</a></li>
+                            <li class="price"><span class="after-disc">99,99 &euro;</span><span class="before-disc"></span></li>
+                        </ul>
+                    </li>
+                    <!-- PRODUCT 6 -->
+                    <li class="card col-flex">
+                        <div class="product">
+                            <img src="/img/6.webp" alt="" class="product-img">
+                            <div class="overlay">
+                                <img src="/img/6b.webp" alt="" class="product-alt-img">
+                            </div>
+                            <div class="favorite">&hearts;</div>
+                            <ul class="badge-container col-flex">
+                                <li class="badge sustainable"><p>Sostenibilità</p></li>
+                            </ul>
+                        </div>
+                        <ul class="product-info">
+                            <li class="product-brand"><a href="#"><p>Esprit</p></a></li>
+                            <li class="product-name"><a href="#">MAGLIONE - BLACK</a></li>
+                            <li class="price"><span class="after-disc">29,99 &euro;</span><span class="before-disc"></span></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div  class="legal-info">
+                    <h3>Boolando s.r.l</h3>
+                    <ul class="nav-menu col-flex">
+                        <li><a href="#">Informazioni legali</a></li>
+                        <li><a href="#">Normativa sulla privacy</a></li>
+                        <li><a href="#">Diritto di recesso</a></li>
+                    </ul>
+                </div>
+                <nav class="footer-nav">
+                    <h5>Trovaci anche su</h5>
+                    <ul class="nav-menu col-flex">
+                        <li><a href="#"><img src="" alt="">🐦</a></li>
+                        <li><a href="#"><img src="" alt="">&#5556;</a></li>
+                        <li><a href="#"><img src="" alt="">&#128247;</a></li>
+                        <li><a href="#"><img src="" alt="">&#42834;</a></li>
+                        <li><a href="#"><img src="" alt="">▶️</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<style scoped>
+
+</style>
